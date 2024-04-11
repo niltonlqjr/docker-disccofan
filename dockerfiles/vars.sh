06786dbb954c6b8515641e9081d959924bf5ab03
niltonlqjr <niltonlqjr@gmail.com>
2024-04-11 18:22:49 -0300

    gitignore and new files organization

--- dockerfiles/vars.sh
+++ dockerfiles/vars.sh
@@ -1,11 +1,12 @@
+VOLUME_DIR="$(realpath $(pwd))/.."  # Mappped volume
+WORK_DIR="$(realpath $(pwd))/.."    # Work dir
 CONTAINER_CMD="docker"              # Container application
 BUILD_CMD="build"                   # Container application build command
 RUN_CMD="run"                       # Container application run command
-DOCKERFILE_NAME="Dockerfile"        # dockerfile name
-DOCKERFILE_PATH="."                 # dockerfile path
+DOCKERFILE_NAME="${WORK_DIR}/dockerfiles/Dockerfile"        # dockerfile name
+CONTEXT_PATH="../"                 # dockerfile context path
 CONTAINER_IMAGE="disccofan:20.04"   # Name of image to be generated
-VOLUME_DIR="$(realpath $(pwd))/.."  # Mappped volume
-WORK_DIR="$(realpath $(pwd))/.."    # Work dir
+
 
 HOST_UID=$(id -u)
 HOST_GID=$(id -g)
