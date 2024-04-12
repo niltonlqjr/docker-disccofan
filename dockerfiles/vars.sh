782a7cfaaf11e246b2e7195cfc0429096505bc20
niltonlqjr <niltonlqjr@gmail.com>
2024-04-12 20:05:51 -0300

    disccofan compile

--- dockerfiles/vars.sh
+++ dockerfiles/vars.sh
@@ -3,7 +3,7 @@ WORK_DIR="$(realpath $(pwd))/.."    # Work dir
 CONTAINER_CMD="docker"              # Container application
 BUILD_CMD="build"                   # Container application build command
 RUN_CMD="run"                       # Container application run command
-DOCKERFILE_NAME="${WORK_DIR}/dockerfiles/Dockerfile"        # dockerfile name
+DOCKERFILE_NAME="${WORK_DIR}/dockerfiles/Dockerfile"       # dockerfile name
 CONTEXT_PATH="../"                 # dockerfile context path
 CONTAINER_IMAGE="disccofan:20.04"   # Name of image to be generated
 
@@ -11,5 +11,5 @@ CONTAINER_IMAGE="disccofan:20.04"   # Name of image to be generated
 HOST_UID=$(id -u)
 HOST_GID=$(id -g)
 
-CONTAINER_BUILD_ARGS="--build-arg UID=$HOST_UID --build-arg GID=$HOST_GID "
+CONTAINER_BUILD_ARGS="--build-arg UID=$HOST_UID --build-arg GID=$HOST_GID --build-arg VOLUME_DIR=$VOLUME_DIR"
 
