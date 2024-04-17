2c81882bd0473e69ca22039307878919b748d596
niltonlqjr <niltonlqjr@gmail.com>
2024-04-17 17:16:48 -0300

    buildx

--- dockerfiles/vars.sh
+++ dockerfiles/vars.sh
@@ -1,7 +1,7 @@
 VOLUME_DIR="$(realpath $(pwd))/.."                      # Mappped volume
 WORK_DIR="$(realpath $(pwd))/.."                        # Work dir
 CONTAINER_CMD="docker"                                  # Container application
-BUILD_CMD="build"                                       # Container application build command
+BUILD_CMD="buildx build"                                       # Container application build command
 RUN_CMD="run"                                           # Container application run command
 DOCKERFILE_NAME="${WORK_DIR}/dockerfiles/Dockerfile"    # dockerfile name
 CONTEXT_PATH="../"                                      # dockerfile context path
