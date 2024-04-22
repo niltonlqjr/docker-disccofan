2b21eb89d4f1b45737c860a442a35d719a7c8b5d
niltonlqjr <niltonlqjr@gmail.com>
2024-04-22 19:26:07 -0300

    update run container

--- dockerfiles/run-container.sh
+++ dockerfiles/run-container.sh
@@ -3,8 +3,6 @@ source ./vars.sh
 #args used to run container
 CONTAINER_RUN_ARGS="-it --rm"
 
-
-
 ${CONTAINER_CMD} ${RUN_CMD} ${CONTAINER_RUN_ARGS} \
   --env "SHELL=/bin/bash" \
   --env "LD_LIBRARY_PATH=/usr/local/hdf5/lib/:$LD_LIBRARY_PATH" \
