a8854cee17b45ca84126ba8c6eb05b0fbe636d2b
Nilton <niltonlqjr@gmail.com>
2024-04-24 17:22:13 -0300

    ssh keys

--- dockerfiles/run-container.sh
+++ dockerfiles/run-container.sh
@@ -2,8 +2,10 @@ source ./vars.sh
 
 #args used to run container
 CONTAINER_RUN_ARGS="-it --rm"
+NETWORK_PORT="-p 2222:22"
 
-${CONTAINER_CMD} ${RUN_CMD} ${CONTAINER_RUN_ARGS} \
+
+${CONTAINER_CMD} ${RUN_CMD} ${CONTAINER_RUN_ARGS} ${NETWORK_PORT}\
   --env "SHELL=/bin/bash" \
   --env "LD_LIBRARY_PATH=/usr/local/hdf5/lib/:$LD_LIBRARY_PATH" \
   --workdir ${VOLUME_DIR} \
