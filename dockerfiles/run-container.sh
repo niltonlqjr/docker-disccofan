44b5bc8b8728ee8cb466b0488a3cee46ab5aef74
Nilton <niltonlqjr@gmail.com>
2024-04-26 15:38:39 -0300

    network

--- dockerfiles/run-container.sh
+++ dockerfiles/run-container.sh
@@ -2,8 +2,8 @@ source ./vars.sh
 
 #args used to run container
 CONTAINER_RUN_ARGS="-it --rm"
-NETWORK_PORT="-p 2222:22 -p 10000-10031:10000-10031/tcp"
-#NETWORK_PORT="--network host"
+#NETWORK_PORT="-p 2222:22 -p 10000-10031:10000-10031/tcp"
+NETWORK_PORT="--network mpi_network"
 
 ${CONTAINER_CMD} ${RUN_CMD} ${CONTAINER_RUN_ARGS} ${NETWORK_PORT}\
   --env "SHELL=/bin/bash" \
