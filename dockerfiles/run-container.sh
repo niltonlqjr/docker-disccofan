765c27c8034ed6039339cbd4d412e8dc26534644
Nilton <niltonlqjr@gmail.com>
2024-05-03 20:22:25 -0300

    network

--- dockerfiles/run-container.sh
+++ dockerfiles/run-container.sh
@@ -3,13 +3,12 @@ source ./vars.sh
 
 #args used to run container
 CONTAINER_RUN_ARGS="-it --rm"
-#NETWORK_PORT="-p 2222:22 -p 10000-10032:10000-10032 -p 11000-11032:11000-11032"
-NETWORK_PORT="--network host"
+NETWORK_PORT="-p 2222:22 -p 52000-52032:52000-52032 -p 53000-53032:53000-53032"
+#NETWORK_PORT="--network host"
 
 ${CONTAINER_CMD} ${RUN_CMD} ${CONTAINER_RUN_ARGS} ${NETWORK_PORT}\
   --env "SHELL=/bin/bash" \
   --env "LD_LIBRARY_PATH=/usr/local/hdf5/lib/:$LD_LIBRARY_PATH" \
-  --env "PATH=/usr/local/bin/:$PATH" \
   --workdir ${HOME} \
   --volume ${HOME}:${HOME} \
   ${CONTAINER_IMAGE}
