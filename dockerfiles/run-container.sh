56094b603da0744d11cfb46bfe2aac99cfd841d6
Nilton <niltonlqjr@gmail.com>
2024-05-03 20:14:14 -0300

    env

--- dockerfiles/run-container.sh
+++ dockerfiles/run-container.sh
@@ -9,6 +9,7 @@ NETWORK_PORT="--network host"
 ${CONTAINER_CMD} ${RUN_CMD} ${CONTAINER_RUN_ARGS} ${NETWORK_PORT}\
   --env "SHELL=/bin/bash" \
   --env "LD_LIBRARY_PATH=/usr/local/hdf5/lib/:$LD_LIBRARY_PATH" \
+  --env "PATH=/usr/local/bin/:$PATH" \
   --workdir ${HOME} \
   --volume ${HOME}:${HOME} \
   ${CONTAINER_IMAGE}
