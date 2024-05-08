f8edccdedcbea5c2b75ecf8b7335328006ac6e54
Nilton <niltonlqjr@gmail.com>
2024-05-08 20:51:05 -0300

    build

--- dockerfiles/0.build-docker.sh
+++ dockerfiles/0.build-docker.sh
@@ -3,9 +3,17 @@ source ./vars.sh
 if [ -n $1 ]
 then
     echo "Building a docker image to a machine that WILL NOT RUN mpirun command"
-else
+elif [ $1 == "mpirun" ]
+then
     echo "Building a docker image to a machine WILL RUN mpirin command"
     DOCKERFILE_NAME='Dockerfile.mpirun'
+else
+    echo "Invalid Argumments"
+    echo "Usage:"
+    echo "$0 [mpirun]"
+    echo "if mpirun argumment used then the docker image will be build to a machine that will run mpirun command"
+    echo "otherwise, the dockerimage will be build to a machine that will NOT run mpirun command"
+    exit 1
 fi
 
 #args used to build dockerfile
