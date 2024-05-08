80db3de4cd16fcc17ac762bc5f4b8de32737b870
Nilton <niltonlqjr@gmail.com>
2024-05-08 20:45:38 -0300

    two configs

--- dockerfiles/0.build-docker.sh
+++ dockerfiles/0.build-docker.sh
@@ -1,5 +1,13 @@
 source ./vars.sh
 
+if [ -n $1 ]
+then
+    echo "Building a docker image to a machine that WILL NOT RUN mpirun command"
+else
+    echo "Building a docker image to a machine WILL RUN mpirin command"
+    DOCKERFILE_NAME='Dockerfile.mpirun'
+fi
+
 #args used to build dockerfile
 CONTAINER_BUILD_ARGS="--build-arg UID=$HOST_UID --build-arg GID=$HOST_GID --build-arg VOLUME_DIR=$VOLUME_DIR"
 
--- dockerfiles/vars.sh
+++ dockerfiles/vars.sh
@@ -1,11 +1,11 @@
-VOLUME_DIR="${HOME}"              # Mappped volume
-WORK_DIR="${HOME}"                # Work dir
-CONTAINER_CMD="docker"            # Container application
-BUILD_CMD="build"                 # Container application build command
-RUN_CMD="run"                     # Container application run command
-DOCKERFILE_NAME="Dockerfile"      # dockerfile name
-CONTEXT_PATH="../"                # dockerfile context path
-CONTAINER_IMAGE="disccofan:20.04" # Name of image to be generated
+VOLUME_DIR="${HOME}"                # Mappped volume
+WORK_DIR="${HOME}"                  # Work dir
+CONTAINER_CMD="docker"              # Container application
+BUILD_CMD="build"                   # Container application build command
+RUN_CMD="run"                       # Container application run command
+DOCKERFILE_NAME="Dockerfile"        # dockerfile name
+CONTEXT_PATH="../"                  # dockerfile context path
+CONTAINER_IMAGE="disccofan:20.04"   # Name of image to be generated
 
 
 HOST_UID=$(id -u) # get user ID from system to use in docker 
