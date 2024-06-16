e604caef352357df07ee94ac78ad9ffa7c53933c
niltonlqjr <niltonlqjr@gmail.com>
2024-06-16 15:02:35 -0300

    split

--- scripts/split_image.py
+++ scripts/split_image.py
@@ -9,10 +9,7 @@ def copia_trecho(image, yini,yfim,xini,xfim,zini,zfim):
     deltaz = zfim-zini
     ret=np.zeros(deltay*deltax*deltaz)
     ret = ret.reshape(deltay,deltax,deltaz)
-    for i in range(yini,yfim):
-        for j in range(xini,xfim):
-            for k in range(zini,zfim):
-                ret[i-yini,j-xini,k-zini] = image[i,j,k]
+    ret[0:deltay,0:deltax,0:deltaz] = image[yini:yfim,xini:xfim,zini:zfim]
     return ret.astype(np.uint8)
 
 parser = argparse.ArgumentParser()
@@ -26,6 +23,7 @@ grid_str = args.grid_dim
 in_prefix, ext = os.path.splitext(filename)
 out_prefix = args.output_prefix
 grid_dims = np.array([int(i) for i in grid_str.split(',')],dtype=np.int64)
+grid_dims[0], grid_dims[1] = grid_dims[1], grid_dims[0]
 
 ndims = len(grid_dims)
 im = skimage.io.imread(filename)
@@ -48,7 +46,7 @@ print(dims)
 print(im.shape)
 
 
-cart_prod=cartesian_prod(offsets)
+#cart_prod=cartesian_prod(offsets)
 
 yinc=dims[0]
 xinc=dims[1]
@@ -58,21 +56,47 @@ count=0
 
 y=0
 while y < im.shape[0]:
-    yini = y-1 if y > 1 else y 
-    yfim = y+yinc if y+yinc < im.shape[0] else im.shape[0]
+    yini = y-1 if y >= 1 else y 
+    yfim = y+yinc+1 if y+yinc+1 < im.shape[0] else im.shape[0]
+    '''
+    if y == 0:
+        if yfim < im.shape[0]:
+            yfim+=1
+    if yfim == im.shape[0]:
+        if yini > 0:
+            yini -=1
+    '''
     x=0
     while x < im.shape[1]:
-        xini = x-1 if x > 1 else x 
-        xfim = x+xinc if x+xinc < im.shape[1] else im.shape[1]
+        xini = x-1 if x >= 1 else x 
+        xfim = x+xinc+1 if x+xinc+1 < im.shape[1] else im.shape[1]
+        '''
+        if x == 0:
+            if xfim < im.shape[1]:
+                xfim+=1
+        if xfim == im.shape[1]:
+            if xini > 0:
+                xini -=1
+        '''
         z=0
         while z < im.shape[2]:
-            zini = z-1 if z > 1 else z 
-            zfim = z+zinc if z+zinc < im.shape[2] else im.shape[2]
-            
+            zini = z-1 if z >= 1 else z 
+            zfim = z+zinc+1 if z+zinc+1 < im.shape[2] else im.shape[2]
+            '''
+            if z == 0:
+                if zfim < im.shape[2]:
+                    zfim+=1
+            if zfim == im.shape[2]:
+                if zini > 0:
+                    zini -=1
+            '''
             im_out = copia_trecho(im,
                                   yini,yfim,
                                   xini,xfim,
                                   zini,zfim)
+            im_out=skimage.color.rgb2gray(im_out)
+            im_out *= 256
+            im_out=im_out.astype(np.uint8)
             imname = f'{out_prefix}-{count}{ext}'
             skimage.io.imsave(imname,im_out)
             print(im_out.shape)
