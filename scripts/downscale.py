83521d693e84510ac8e3f25f2a88b8485602db90
niltonlqjr <niltonlqjr@gmail.com>
2024-06-22 04:58:41 -0300

    imageio

--- scripts/downscale.py
+++ scripts/downscale.py
@@ -2,24 +2,31 @@ import skimage as sk
 import numpy as np
 import os
 import argparse
+import imageio.v3 as iio
+import imageio_freeimage
+
 
 parser = argparse.ArgumentParser()
-parser.add_argument('filename', type = str)
-parser.add_argument('--factor', '-f', dest='downsample_factor', default='2,2', type=str)
-parser.add_argument('--output-file', '-o', dest='output_file', default='ds.jpg',type=str)
+parser.add_argument('filename', type = str, help='input file')
+parser.add_argument('--factor', '-f', dest='downsample_factor', default='2,2', type=str, help='downsample scale using comma as separator')
+parser.add_argument('--output-file', '-o', dest='output_file', default='ds.jpg',type=str,help='ouput filename with extension')
 parser.add_argument('--verbose', dest='verbose', action='store_true')
+parser.add_argument('--library', '-l', dest='library', default='skimage',help='library to read image [skimage(default) | imageio]\n imageio uses freeimage')
 args=parser.parse_args()
 
 filename = args.filename
 dsf_str = args.downsample_factor
-verbose = args.verbose
 out_name = args.output_file
+lib = args.library
 
 in_prefix, ext = os.path.splitext(filename)
 
 factor = tuple(int(x) for x in dsf_str.split(','))
 
-im = sk.io.imread(filename)
+if lib == 'skimage':
+    im = sk.io.imread(filename)
+elif lib == 'imageio':
+    im = iio.imread(filename,plugin='JP2-FI')
 
 ds = sk.transform.downscale_local_mean(im,factor)
 ds = np.round(ds)
