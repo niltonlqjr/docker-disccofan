0600a3e63694b3f32087e2cea9649a432d6a7b27
niltonlqjr <niltonlqjr@gmail.com>
2024-07-02 20:19:18 -0300

    downscale c

--- Downscale/downscale.c
+++ Downscale/downscale.c
@@ -6,12 +6,12 @@
 #include <stdbool.h>
 
 
-struct Argumments{
+struct Arguments{
     int *factors;
     char *filename;
     char *output_file;
     char *img_ext;
-} program_args;
+};
 
 void upcase_string(char *str){
     int i;
@@ -19,6 +19,8 @@ void upcase_string(char *str){
         str[i] = toupper(str[i]);
     }
 }
+
+
 
 void vector_from_string(char *str, char sep, int **ret, int *size){
     int i, *vec;
@@ -47,7 +49,7 @@ void vector_from_string(char *str, char sep, int **ret, int *size){
 @return Returns the loaded dib if successful, returns NULL otherwise
 */
 FIBITMAP* GenericLoader(const char* lpszPathName, int flag,  bool convertToGreyScale) {
-    FIBITMAP *tmp, *dib;
+    FIBITMAP *tmp = NULL, *dib;
     FREE_IMAGE_FORMAT fif = FIF_UNKNOWN;
     // check the file signature and deduce its format
     // (the second argument is currently not used by FreeImage)
@@ -64,7 +66,7 @@ FIBITMAP* GenericLoader(const char* lpszPathName, int flag,  bool convertToGreyS
         // unless a bad file format, we are done !
     }
     dib=tmp;
-    if(convertToGreyScale){
+    if(convertToGreyScale && tmp){
         dib = FreeImage_ConvertToGreyscale(tmp);
         FreeImage_Unload(tmp);
     }
@@ -90,29 +92,29 @@ bool GenericSaver(FIBITMAP* img, const char *path, int flag){
     return false;
 }
 
+
 static int parse_args(int key, char *arg, struct argp_state *state){
+
+    struct Arguments *program_args = state->input;
     int *dims, dims_size, len;
+
     if(key == 'f'){
         vector_from_string(arg, ',', &dims, &dims_size);
         if(dims_size != 2){
             printf("The number of dimensions of downscale factor must be 2");
             exit(0);
         }
-        program_args.factors = dims;
-    }else if(key == 'i'){
-        len = strlen(arg);
-        program_args.filename = (char*) malloc(len * sizeof(char));
-        strncpy(program_args.filename, arg, len);
+        program_args->factors = dims;
     }else if(key == 'o'){
         len = strlen(arg);
-        program_args.output_file = (char*) malloc(len * sizeof(char));
-        strncpy(program_args.output_file, arg, len);
-    }else if(key == 'e'){
-        printf("%s",arg);
+        program_args->output_file = (char*) malloc(len * sizeof(char));
+        strncpy(program_args->output_file, arg, len);
+    }else if(key == ARGP_KEY_ARG){
         len = strlen(arg);
-        program_args.img_ext = (char*) malloc(len *sizeof(char));
-        strncpy(program_args.img_ext, arg, len);
-        upcase_string(program_args.img_ext);
+        program_args->filename = (char*) malloc(len * sizeof(char));
+        strncpy(program_args->filename, arg, len);
+    }else if(key == ARGP_KEY_NO_ARGS){
+        argp_usage(state);
     }
     return 0;
 }
@@ -121,15 +123,19 @@ static int parse_args(int key, char *arg, struct argp_state *state){
 int main(int argc, char **argv){
     struct argp_option args_opts[] = {
         {0,'f',"DIMS",0,"Downscale factor comma separated"},
-        {0,'i',"FILENAME",0,"Input file name"},
         {0,'o',"FILENAME",0,"Output file name"},
-        {0,'e', "TYPE", 0, "Input and output format"},
         {0}
     };
+    int default_dim[] = {2,2};
+
+    struct Arguments program_args;
+
+    program_args.factors = default_dim;
+    program_args.output_file = "saida.png";
 
     struct argp args = {args_opts, parse_args, 0, 0};
     
-    argp_parse(&args, argc, argv, 0, 0, 0);
+    argp_parse(&args, argc, argv, 0, 0, &program_args);
     
     char *filename = program_args.filename;
     char *output = program_args.output_file;
@@ -139,7 +145,6 @@ int main(int argc, char **argv){
     FIBITMAP *img, *rescale;
 
     printf("Input:%s \nOutput:%s\n",filename,output);
-    printf("Image extension:%s\n", img_ext);
     printf("Reduction factor: (%d,%d)\n", factors[0], factors[1]);
 
     img = GenericLoader(filename,0,true);
@@ -151,6 +156,9 @@ int main(int argc, char **argv){
 
     unsigned int img_w = FreeImage_GetWidth(img);
     unsigned int img_h = FreeImage_GetHeight(img);
+
+    printf("Rescaling from (%d, %d) to (%d, %d)\n",img_w,img_h,
+                        img_w/factors[0], img_h/factors[1]);
 
     rescale = FreeImage_Rescale(img, img_w/factors[0], img_h/factors[1],FILTER_BOX);
     FreeImage_Unload(img);
