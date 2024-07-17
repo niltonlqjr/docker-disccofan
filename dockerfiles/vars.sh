14b46084fd5e25d009e19afc573d96676a923a39
niltonlqjr <niltonlqjr@gmail.com>
2024-07-16 23:11:19 -0300

    fix dockerfile

--- dockerfiles/vars.sh
+++ dockerfiles/vars.sh
@@ -1,9 +1,11 @@
-VOLUME_DIR="${HOME}"                # Mappped volume
-WORK_DIR="${HOME}"                  # Directory that will be used to executables
-CONTAINER_CMD="docker"              # Container application
-BUILD_CMD="build"                   # Container application build command
-CONTEXT_PATH="../"                  # dockerfile context path
-CONTAINER_IMAGE="disccofan:20.04"   # Name of image to be generated
+VOLUME_DIR="${HOME}"                            # Mappped volume
+WORK_DIR="${HOME}"                              # Directory that will be used to executables
+CONTAINER_CMD="docker"                          # Container application
+BUILD_CMD="build"                               # Container application build command
+CONTEXT_PATH="../"                              # dockerfile context path
+IMAGE_NAME="disccofan"                          # name of the container
+IMAGE_VERSION="22.04"                           # version of image
+CONTAINER_IMAGE=${IMAGE_NAME}:${IMAGE_VERSION}  # Name of image to be generated
 CONTAINER_NAME="DISCCOFAN"
 
 HOST_UID=$(id -u) # get user ID from system to use in docker 
