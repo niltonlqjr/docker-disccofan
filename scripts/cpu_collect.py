bcb435c7205e2f9002213c198da11a6a1ee54352
niltonlqjr <niltonlqjr@gmail.com>
2024-08-08 16:46:51 -0300

    collect more cpu data

--- scripts/cpu_collect.py
+++ scripts/cpu_collect.py
@@ -10,6 +10,7 @@ class DataProcess:
         self.ps: psutil.Process = psutil_process
         self.mem_usage: list[int] = []
         self.cpu_usage: list[float] = []
+        self.cpu_times: psutil._pslinux.pcputimes = None
 
     def update_ps(self):
         try:
@@ -22,7 +23,10 @@ class DataProcess:
         if verbose:
             print(f'inserting {usage} into usage list of {self.ps.name()}')
         self.cpu_usage.append(usage)
-            
+        times = self.ps.cpu_times()
+        if verbose:
+            print(f'inserting cpu times: {times} of {self.ps.name()}')
+        self.cpu_times = times
     
     def insert_mem_usage(self, verbose=False):
         mem_info = self.ps.memory_full_info()
@@ -36,19 +40,32 @@ class DataProcess:
         self.mem_usage.append(mem)
 
 
-    def save_cpu_usage(self, filename: str, verbose=False) -> bool:
+    def save_cpu_usage(self, filename: str, verbose=False, header=False) -> bool:
         try:
             if verbose:
                 print(f'saving cpu usage for process {self.ps.pid}')
-            with open(f'{filename}-{self.ps.pid}.txt','a') as f:
-                for u in self.cpu_usage:
-                    f.write(str(u)+'\n')
+            with open(f'{filename}-CPU_PERCENT-{self.ps.pid}.txt','a') as f:
+                if header:
+                    f.write('cpu %\n')
+                for i in range(len(self.cpu_usage)):
+                    u = self.cpu_usage[i]
+                    f.write(f'{u}\n')
             self.cpu_usage = []
             return True
         except:
             print(f'error saving cpu usage for pid: {self.ps.pid}',file=sys.stderr)
             return False
-        
+
+    def save_times(self, filename: str, verbose=False, header=True):
+        with open(f'{filename}-TIMES-{self.ps.pid}.txt','w') as f:
+            times=self.cpu_times
+            if header:
+                f.write('User\tSystem\tChildren User\tChildren System\tIO wait\n')
+            f.write('{user}\t{system}\t{c_user}\t{c_system}\t{io}\n'.format(
+                user=times.user, system=times.system, c_user=times.children_user,
+                c_system=times.children_system, io=times.iowait)
+            )
+    
     def save_mem_usage(self, filename: str, verbose=False, header=False) -> bool:
         try:
             if verbose:
@@ -90,21 +107,22 @@ class ProcessorMonitor:
         #try:
         if verbose:
             print(f'saving system cpu usage')
-        
-        for cpu_id in range(len(self.m[0])):
-            with open(f'{filename}-SystemCPU-{cpu_id}.txt','a') as f:
-                if header:
-                    f.write('User\tNice\tSystem\tIdle\
-                            \tio wait\tHardware Interrupts (irq)\tSoftware Interrupts (softirq)\t\
-                            Steal by other OSs\tGuest\tGuest Nice\n')
-                for m in self.m:
-                    f.write("{user}\t{nice}\t{system}\t{idle}\t{iowait}\t{irq}\t{softirq}\t{steal}\t{guest}\t{guest}\t{guest_nice}\n".format(
-                        user=m[cpu_id].user,nice=m[cpu_id].nice,system=m[cpu_id].system,idle=m[cpu_id].idle,
-                        iowait=m[cpu_id].iowait,irq=m[cpu_id].irq,softirq=m[cpu_id].softirq,
-                        steal=m[cpu_id].steal,guest=m[cpu_id].guest,guest_nice=m[cpu_id].guest_nice)
-                    )
-        self.m = []
-        return True
+        if len(self.m) > 0:
+            for cpu_id in range(len(self.m[0])):
+                with open(f'{filename}-SystemCPU-{cpu_id}.txt','a') as f:
+                    if header:
+                        f.write('User\tNice\tSystem\tIdle\
+                                \tio wait\tHardware Interrupts (irq)\tSoftware Interrupts (softirq)\t\
+                                Steal by other OSs\tGuest\tGuest Nice\n')
+                    for m in self.m:
+                        f.write("{user}\t{nice}\t{system}\t{idle}\t{iowait}\t{irq}\t{softirq}\t{steal}\t{guest}\t{guest}\t{guest_nice}\n".format(
+                            user=m[cpu_id].user,nice=m[cpu_id].nice,system=m[cpu_id].system,idle=m[cpu_id].idle,
+                            iowait=m[cpu_id].iowait,irq=m[cpu_id].irq,softirq=m[cpu_id].softirq,
+                            steal=m[cpu_id].steal,guest=m[cpu_id].guest,guest_nice=m[cpu_id].guest_nice)
+                        )
+            return True
+        else:
+            return False
         #except:
         #    print(f'error cpu usage',file=sys.stderr)
         #    return False
@@ -113,9 +131,9 @@ class ProcessorMonitor:
 #############       Main begins here      ##############
 
 parser = argparse.ArgumentParser(description='Collect cpu usage and memory consumption of monitored program.\n\
-                                 The cpu usage is collected by process (<cpu-output-prefix>-pid.txt) and by \
-                                 processor(<cpu-output-prefix>-SystemCPU-ProcessorID.txt).\n\
-                                 The memory consumption is collect by process (<memory-output-prexix>-pid.txt)')
+                                 The cpu usage is collected by process (<cpu-output-prefix>-*-<pid>.txt) and by \
+                                 processor(<cpu-output-prefix>-SystemCPU-<ProcessorID>.txt).\n\
+                                 The memory consumption is collect by process (<memory-output-prexix>-<pid>.txt)')
 
 parser.add_argument('monitored_name', type=str, default='a.out',
                     help='Process that the program will monitor cpu and memory consumption')
@@ -184,7 +202,7 @@ while monitored_pids == {}:
 if verbose:
     print(f"Process {monitored_name} START!")
 
-
+print(write_header)
 
 while monitored_pids != {}:
     sleep(interval_time)
@@ -198,7 +216,7 @@ while monitored_pids != {}:
         
     for p in psutil.process_iter():
         try:
-            if p.username() == me and p.name() == monitored_name:
+            if p.name() == monitored_name:
                 if not(p.pid in procs):
                     procs[p.pid] = DataProcess(p)
                     monitored_pids[p.pid] = True
@@ -206,12 +224,15 @@ while monitored_pids != {}:
                 procs[p.pid].insert_cpu_usage(verbose=verbose)
                 procs[p.pid].insert_mem_usage(verbose=verbose)
                 if len(procs[p.pid].cpu_usage) > buffer_size:
+                    print(write_header)
                     procs[p.pid].save_mem_usage(out_file_mem,
                                                 verbose=verbose,
                                                 header=write_header[p.pid])
                     procs[p.pid].save_cpu_usage(out_file_cpu,
-                                                verbose=verbose)
+                                                verbose=verbose,
+                                                header=write_header[p.pid])
                     write_header[p.pid] = write_header[p.pid] and False
+                    
         except:
             print(f'error in process {p.pid}', file=sys.stderr)
     pop_vals = []
@@ -223,7 +244,14 @@ while monitored_pids != {}:
 
 for p in procs:
     procs[p].save_cpu_usage(out_file_cpu,
-                            verbose=verbose)
+                            verbose=verbose,
+                            header=write_header[p])
     procs[p].save_mem_usage(out_file_mem,
-                            verbose=verbose)
+                            verbose=verbose,
+                            header=write_header[p])
+    procs[p].save_times(out_file_cpu,
+                        verbose=verbose)
+processor.save_measures(out_file_cpu,
+                        verbose=verbose,
+                        header=proc_header)
 
