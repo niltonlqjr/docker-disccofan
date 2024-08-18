b4c370f90685bbc71d2e9e0adb0bed35bc17aef6
niltonlqjr <niltonlqjr@gmail.com>
2024-08-18 14:11:52 -0300

    fix run scripts

--- scripts/run-cpu-collect.sh
+++ scripts/run-cpu-collect.sh
@@ -1,9 +1,8 @@
 dims=('4 4' '8 4' '8 6' '8 8' '10 8' '12 8' '14 8' '16 8' '12 12' '16 10' '16 11' '16 12')
 lim=`expr ${#dims[@]} - 1`
-lim_proc_host=12
 
 script=/home/mpi/cpu_collect.py
-NUM_EXECS=5
+NUM_EXECS=3
 
 OUT_PREFIX=/home/mpi/host/results-disccofan
 
@@ -13,15 +12,13 @@ do
     d1=`echo $dim | awk '{split($0,a," "); print a[1]}'`
     d2=`echo $dim | awk '{split($0,a," "); print a[2]}'`
     num_proc=`expr ${d1} '*' ${d2}`
-    for PROC_PER_HOST in `seq 1 ${lim_proc_host}`
+    PROC_PER_HOST=`expr i + 1`
+    mkdir -p ${OUT_PREFIX}/dim_${d1}-${d2}-1/proc_per_host${PROC_PER_HOST}/
+    for ((exec=1;$exec<=$NUM_EXECS;exec+=1))
     do
-        mkdir -p ${OUT_PREFIX}/dim_${d1}-${d2}-1/proc_per_host${PROC_PER_HOST}/
-        for ((exec=1;$exec<=3;exec+=1))
-        do
-            output_complete_dir=${OUT_PREFIX}/dim_${d1}-${d2}-1/proc_per_host${PROC_PER_HOST}/
-            echo "grid: ${d1},${d2},1 => np: ${num_proc}, per host: ${PROC_PER_HOST} , ${exec}"
-            python3 $script -p -c ${output_complete_dir}/cpu_out_exec${exec}.txt -m ${output_complete_dir}/mem_out_exec${exec}.txt -i 0.5 disccofan
-        done
+        output_complete_dir=${OUT_PREFIX}/dim_${d1}-${d2}-1/proc_per_host${PROC_PER_HOST}/
+        echo "grid: ${d1},${d2},1 => np: ${num_proc}, per host: ${PROC_PER_HOST} , ${exec}"
+        python3 $script -p -c ${output_complete_dir}/cpu_out_exec${exec}.txt -m ${output_complete_dir}/mem_out_exec${exec}.txt -i 0.5 disccofan
     done
 done
 
--- scripts/run-disccofan.sh
+++ scripts/run-disccofan.sh
@@ -1,7 +1,7 @@
 dims=( '4 4' '8 4' '8 6' '8 8' '10 8' '12 8' '14 8' '16 8' '12 12' '16 10' '16 11' '16 12' )
 lim=`expr ${#dims[@]} - 1`
-lim_proc_host=12
-NUM_EXECS=5
+
+NUM_EXECS=3
 
 INP_DIR=/home/mpi/host/split
 
@@ -13,16 +13,14 @@ do
     d1=`echo $dim | awk '{split($0,a," "); print a[1]}'`
     d2=`echo $dim | awk '{split($0,a," "); print a[2]}'`
     in_prefix_img=${INP_DIR}/dim_${d1}-${d2}-1/IMG_PHR1A__${d1}_${d2}_1
-    for PROC_PER_HOST in `seq 1 ${lim_proc_host}`
+    PROC_PER_HOST=`expr i + 1`
+    HOSTFILE=${HF_DIR}/hosts_lins_${PROC_PER_HOST}proc.txt
+    for ((exec=1;$exec<=$NUM_EXECS;exec+=1))
     do
-        HOSTFILE=${HF_DIR}/hosts_lins_${PROC_PER_HOST}proc.txt
-        for ((exec=1;$exec<=3;exec+=1))
-        do
-            num_proc=`expr ${d1} '*' ${d2}`
-            echo "grid: ${d1},${d2},1 => np: ${num_proc}, per host: ${PROC_PER_HOST}, exec=${exec}"
-            mpirun -np ${num_proc} --hostfile ${HOSTFILE} ./disccofan -g ${d1},${d2},1 --inprefix ${in_prefix_img} --intype JP2 --infile 1 --overlap 0
-            sleep 3
-        done
+        num_proc=`expr ${d1} '*' ${d2}`
+        echo "grid: ${d1},${d2},1 => np: ${num_proc}, per host: ${PROC_PER_HOST}, exec=${exec}"
+        mpirun -np ${num_proc} --hostfile ${HOSTFILE} ./disccofan -g ${d1},${d2},1 --inprefix ${in_prefix_img} --intype JP2 --infile 1 --overlap 0
+        sleep 5
     done
 done
 
