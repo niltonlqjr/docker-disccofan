45dc70a52bc2de1bc32e2e5ba23e252885310990
niltonlqjr <niltonlqjr@gmail.com>
2024-08-18 04:29:50 -0300

    run scripts

--- scripts/run-network-collect.sh
+++ scripts/run-network-collect.sh
@@ -1,6 +1,6 @@
 dims=('10 8' '12 8' '14 8' '16 11' '16 8' '4 4' '8 4' '8 8' '12 12' '16 10' '16 12' '8 6' )
 lim=`expr ${#dims[@]} - 1`
-
+lim_proc_host=12
 
 script=/home/mpi/net_collect.py
 NUM_EXECS=5
@@ -13,7 +13,7 @@ do
     d1=`echo $dim | awk '{split($0,a," "); print a[1]}'`
     d2=`echo $dim | awk '{split($0,a," "); print a[2]}'`
     num_proc=`expr ${d1} '*' ${d2}`
-    for PROC_PER_HOST in `seq 1 12`
+    for PROC_PER_HOST in `seq 1 ${lim_proc_host}`
     do
         mkdir -p ${OUT_PREFIX}/dim_${d1}-${d2}-1/proc_per_host${PROC_PER_HOST}/
         for ((exec=1;$exec<=3;exec+=1))
