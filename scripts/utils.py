7a310bedeef5b03b9a2dc67f1440f6dfc1466489
niltonlqjr <niltonlqjr@gmail.com>
2024-08-24 16:53:49 -0300

    utils

--- scripts/utils.py
+++ scripts/utils.py
@@ -3,7 +3,7 @@ def lines_of(string, sep='\n'):
     return string.split(sep)
 
 @staticmethod
-def line_to_list(line, columns_types, header=True):
+def line_to_list(line, columns_types):
     spl = line.split()
     ncolum=len(columns_types)
     if len(spl) != ncolum:
@@ -11,3 +11,11 @@ def line_to_list(line, columns_types, header=True):
         return []
     ret = [columns_types[i](spl[i]) for i in range(ncolum)]
     return ret
+
+@staticmethod
+def text_table_to_data(str_table, colum_types, header=True ):
+    lines = lines_of(str_table)
+    if header:
+        del lines[0]
+    data = [line_to_list(l,columns_types=colum_types) for l in lines if l!='']
+    return data
