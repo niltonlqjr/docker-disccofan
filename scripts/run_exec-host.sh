72708d3645867057d2fc31cca954348680cb8b40
Nilton <niltonlqjr@gmail.com>
2026-07-15 04:32:15 -0300

    scripts

---
+++ scripts/run_exec-host.sh
@@ -0,0 +1,26 @@
+dir=$1
+pass=$2
+
+
+if [ -z $1 ]
+then
+    echo "Usage: $0 directory password"
+    echo "where <directory> is the directory that contains scritp to start exec-host.sh"
+    exit 1
+fi
+
+if [ -z $2 ]
+then
+    echo "Usage: $0 directory password"
+    exit 1
+fi
+
+if [ ! -f "${dir}/exec-host.sh" ]
+then
+    echo "file ${dir}/exec-host.sh not found"
+    exit 1
+fi
+
+cd ${dir}
+${pass} | sudo -S pwd
+./exec-host.sh
