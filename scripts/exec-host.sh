26636bdd342460dbb0a07231b552d7ede9f9ae1b
Nilton <niltonlqjr@gmail.com>
2026-07-19 00:47:48 -0300

    update exec host

--- scripts/exec-host.sh
+++ scripts/exec-host.sh
@@ -1,5 +1,22 @@
-FILE_CMD='container_command.txt'
-sudo ls
+FILE_CMD=$1
+pass=$2
+
+if [ -z $1 ]
+then
+    echo "Usage: $0 <comand file> <password>"
+    echo "where <command file> is the the file with command to be executed"
+    exit 1
+fi
+
+if [ -z $2 ]
+then
+    echo "Usage: $0 <comand file> <password>"
+    echo "where <command file> is the the file with command to be executed"
+    exit 1
+fi
+
+echo ${pass} | sudo -S ls
+
 while true;
 do
     sleep 1
