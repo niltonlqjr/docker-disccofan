05bfc9f758e9f2722dac84e9c45a5e99e9e56933
Nilton <niltonlqjr@gmail.com>
2026-07-31 03:16:45 -0300

    script disccofan

--- scripts/run_disccofan_single.sh
+++ scripts/run_disccofan_single.sh
@@ -7,4 +7,4 @@ out_prefix=host/gigapixel_img/PHR1A/200_tiles_filtered/dim_${grid}/IMG_PHR1A__${
 in_type=JP2
 
 
-mpirun -np ${np} --hostfile ${hf} disccofan/disccofan -g ${grid} --inprefix ${in_prefix} --intype ${in_type} --outprefix ${out_prefix} --infile 1 --overlap 0
+mpirun -np ${np} --hostfile ${hf} disccofan/disccofan -g ${grid_comma} --inprefix ${in_prefix} --intype ${in_type} --outprefix ${out_prefix} --infile 1 --overlap 0
