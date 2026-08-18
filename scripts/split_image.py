b5e2072c2f6fa21cb3b3de0fe4e854c713301412
Nilton <niltonlqjr@gmail.com>
2026-08-17 21:17:34 -0300

    split image split 2d images

--- scripts/split_image.py
+++ scripts/split_image.py
@@ -10,7 +10,7 @@ from PIL import Image, ImageFile
 Image.MAX_IMAGE_PIXELS = None
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
-def copia_trecho(image, xini,xfim,yini,yfim,zini,zfim):
+def copia_trecho_rgb(image, xini,xfim,yini,yfim,zini,zfim):
     deltax = xfim-xini
     deltay = yfim-yini
     deltaz = zfim-zini
@@ -22,6 +22,18 @@ def copia_trecho(image, xini,xfim,yini,yfim,zini,zfim):
     ret[:,:,:] = image[yini:yfim,xini:xfim,zini:zfim]
     return ret.astype(np.uint8)
 
+def copia_trecho_gs(image, xini,xfim,yini,yfim):
+    deltax = xfim-xini
+    deltay = yfim-yini
+    
+    if verbose:
+        print('delta:',deltax,deltay)
+        print(xini,xfim,yini,yfim)
+    ret=np.zeros(deltax*deltay)
+    ret = ret.reshape(deltay,deltax)
+    ret[:,:] = image[yini:yfim,xini:xfim]
+    return ret.astype(np.uint8)
+
 def print_mat(im,dim):
     for i in range(len(im)):
         for j in range(len(im[i])):
@@ -30,6 +42,159 @@ def print_mat(im,dim):
             elif dim == 2:
                 print('{:4d}'.format(im[i,j]),end='')
         print('')
+
+def run_rgb():
+    global ndims
+    global im
+    global grid_dims
+    
+    dims_T = np.zeros(ndims, dtype=np.int64)
+
+    offsets = np.zeros(len(grid_dims), dtype=np.int64)
+
+
+    for d in range(ndims):
+        dims_T[d] = int(im.shape[d])#//grid_dims[d]
+        offsets[d] = 0
+    if verbose:
+        print('offsets:',offsets)
+        print('dims_T:',dims_T)
+        print(im.shape)
+        #print_mat(im, 3)
+
+    count=0
+    dims = np.arange(ndims, dtype=np.int64)
+
+
+    for tile in range(grid_dims[0]*grid_dims[1]*grid_dims[2]):
+        if tile != grid_dims[0]*grid_dims[1]*grid_dims[2] - 1:
+            myrank = (tile * grid_dims[0]) % (grid_dims[0] * grid_dims[1] -1)
+        else:
+            myrank = tile 
+        myrank_2D = myrank % (grid_dims[1]*grid_dims[0])
+        myrank_arr = [myrank_2D % grid_dims[0], myrank_2D // grid_dims[0], myrank // (grid_dims[1]*grid_dims[0])]
+        #if verbose:
+        print(f'tile {tile} at {[int(myrank_arr[i]) for i in range(len(myrank_arr))]} with rank {myrank}')
+        for i in range(2):
+            dims[i]  = dims_T[i]//grid_dims[i]
+            offsets[i] = myrank_arr[i] * dims[i]
+            if myrank_arr[i] < dims_T[i]%grid_dims[i]:
+                dims[i] += 1
+                offsets[i] += myrank_arr[i]
+            else:
+                offsets[i] += dims_T[i]%grid_dims[i]
+            if (offsets[i] > 0) and overlap:
+                offsets[i] -= 1
+                dims[i] += 1
+            if (dims[i] + offsets[i] != dims_T[i]) and overlap:
+                dims[i]+=1
+        print(f'offsets={offsets}')
+        print(f'dims={dims}')
+        xini = offsets[1]
+        yini = offsets[0]
+        xfim = offsets[1] + dims[1]
+        yfim = offsets[0] + dims[0]
+        zini = 0
+        zfim = im.shape[2]
+        if verbose:
+            print(' x, y, z ranges:',xini,xfim,yini,yfim,zini,zfim)
+        im_out = copia_trecho_rgb(im,
+                            xini,xfim,
+                            yini,yfim,
+                            zini,zfim)
+        
+        imname = f'{out_prefix}-{tile}{ext}'
+
+
+        #im_out=skimage.color.rgb2gray(im_out)
+        #im_out *= 255
+        #im_out=im_out.astype(np.uint8)
+        
+        if plugin == 'skimage':
+            skimage.io.imsave(imname,im_out)
+        elif plugin == 'imageio':
+            iio.imwrite(imname,im_out)
+        
+        if verbose:
+            print(imname)
+            #print_mat(im_out,2)
+            print(im_out.shape)
+
+
+def run_gs():
+    global ndims
+    global im
+    global grid_dims
+
+
+    dims_T = np.zeros(ndims, dtype=np.int64)
+    
+    offsets = np.zeros(len(grid_dims), dtype=np.int64)
+
+    for d in range(ndims):
+        dims_T[d] = int(im.shape[d])#//grid_dims[d]
+        offsets[d] = 0
+    if verbose:
+        print('offsets:',offsets)
+        print('dims_T:',dims_T)
+        print(im.shape)
+        #print_mat(im, 3)
+
+    count=0
+    dims = np.arange(ndims, dtype=np.int64)
+
+
+    for tile in range(grid_dims[0]*grid_dims[1]):
+        if tile != grid_dims[0]*grid_dims[1] - 1:
+            myrank = (tile * grid_dims[0]) % (grid_dims[0] * grid_dims[1] -1)
+        else:
+            myrank = tile 
+        myrank_2D = myrank % (grid_dims[1]*grid_dims[0])
+        myrank_arr = [myrank_2D % grid_dims[0], myrank_2D // grid_dims[0], myrank // (grid_dims[1]*grid_dims[0])]
+        #if verbose:
+        print(f'tile {tile} at {[int(myrank_arr[i]) for i in range(len(myrank_arr))]} with rank {myrank}')
+        for i in range(2):
+            dims[i]  = dims_T[i]//grid_dims[i]
+            offsets[i] = myrank_arr[i] * dims[i]
+            if myrank_arr[i] < dims_T[i]%grid_dims[i]:
+                dims[i] += 1
+                offsets[i] += myrank_arr[i]
+            else:
+                offsets[i] += dims_T[i]%grid_dims[i]
+            if (offsets[i] > 0) and overlap:
+                offsets[i] -= 1
+                dims[i] += 1
+            if (dims[i] + offsets[i] != dims_T[i]) and overlap:
+                dims[i]+=1
+        print(f'offsets={offsets}')
+        print(f'dims={dims}')
+        xini = offsets[1]
+        yini = offsets[0]
+        xfim = offsets[1] + dims[1]
+        yfim = offsets[0] + dims[0]
+        if verbose:
+            print(' x, y, z ranges:',xini,xfim,yini,yfim,zini,zfim)
+        im_out = copia_trecho_gs(im,
+                            xini,xfim,
+                            yini,yfim,)
+        
+        imname = f'{out_prefix}-{tile}{ext}'
+
+
+        #im_out=skimage.color.rgb2gray(im_out)
+        #im_out *= 255
+        #im_out=im_out.astype(np.uint8)
+        
+        if plugin == 'skimage':
+            skimage.io.imsave(imname,im_out)
+        elif plugin == 'imageio':
+            iio.imwrite(imname,im_out)
+        
+        if verbose:
+            print(imname)
+            #print_mat(im_out,2)
+            print(im_out.shape)
+
 
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('filename', type = str)
@@ -92,81 +257,18 @@ elif plugin == 'imageio':
 else:
     print("invalid plugin")
     exit()
-    
+
 if len(im.shape) != ndims:
     print("Invalid dimensions")
+    print(f"im.shape = {im.shape}")
+    print(f"im.shape = {ndims}")
     exit()
 
-dims_T = np.zeros(ndims, dtype=np.int64)
 
-offsets = np.zeros(len(grid_dims), dtype=np.int64)
-
-
-for d in range(ndims):
-    dims_T[d] = int(im.shape[d])#//grid_dims[d]
-    offsets[d] = 0
-if verbose:
-    print('offsets:',offsets)
-    print('dims_T:',dims_T)
-    print(im.shape)
-    #print_mat(im, 3)
-
-count=0
-dims = np.arange(ndims, dtype=np.int64)
-
-for tile in range(grid_dims[0]*grid_dims[1]*grid_dims[2]):
-    if tile != grid_dims[0]*grid_dims[1]*grid_dims[2] - 1:
-        myrank = (tile * grid_dims[0]) % (grid_dims[0] * grid_dims[1] -1)
-    else:
-        myrank = tile 
-    myrank_2D = myrank % (grid_dims[1]*grid_dims[0])
-    myrank_arr = [myrank_2D % grid_dims[0], myrank_2D // grid_dims[0], myrank // (grid_dims[1]*grid_dims[0])]
-    #if verbose:
-    print(f'tile {tile} at {[int(myrank_arr[i]) for i in range(len(myrank_arr))]} with rank {myrank}')
-    for i in range(2):
-        dims[i]  = dims_T[i]//grid_dims[i]
-        offsets[i] = myrank_arr[i] * dims[i]
-        if myrank_arr[i] < dims_T[i]%grid_dims[i]:
-            dims[i] += 1
-            offsets[i] += myrank_arr[i]
-        else:
-            offsets[i] += dims_T[i]%grid_dims[i]
-        if (offsets[i] > 0) and overlap:
-            offsets[i] -= 1
-            dims[i] += 1
-        if (dims[i] + offsets[i] != dims_T[i]) and overlap:
-            dims[i]+=1
-    print(f'offsets={offsets}')
-    print(f'dims={dims}')
-    xini = offsets[1]
-    yini = offsets[0]
-    xfim = offsets[1] + dims[1]
-    yfim = offsets[0] + dims[0]
-    zini = 0
-    zfim = im.shape[2]
-    if verbose:
-        print(' x, y, z ranges:',xini,xfim,yini,yfim,zini,zfim)
-    im_out = copia_trecho(im,
-                          xini,xfim,
-                          yini,yfim,
-                          zini,zfim)
-    
-    imname = f'{out_prefix}-{tile}{ext}'
-
-
-    #im_out=skimage.color.rgb2gray(im_out)
-    #im_out *= 255
-    #im_out=im_out.astype(np.uint8)
-    
-    if plugin == 'skimage':
-        skimage.io.imsave(imname,im_out)
-    elif plugin == 'imageio':
-        iio.imwrite(imname,im_out)
-    
-    if verbose:
-        print(imname)
-        #print_mat(im_out,2)
-        print(im_out.shape)
+if ndims == 3:
+    run_rgb()
+elif ndims == 2:
+    run_gs()
     
 '''
 image.c
